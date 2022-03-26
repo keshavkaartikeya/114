02fff89505d096bc2d0b280d9d91e3c91d924527
keshavkaartikeya <90041638+keshavkaartikeya@users.noreply.github.com>
2022-03-26 08:45:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(360,360);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("MyMustache.png");
+}
